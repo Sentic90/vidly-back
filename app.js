cb954079d4cb7325deedb19dec2ebf93f4ca69cd
sentic90 <princeali5549@gmail.com>
2023-06-07 10:17:42 +0200

    added User unit Test

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-fetch("http://localhost:8000/store/products/")
-  .then((res) => console.log(res.data))
-  .catch((err) => console.log(err));
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { User } = require("./models/user");
 
+const userInstance = {
+  name: "abcde",
+  email: "abc@domain.com",
+  password: "12345",
+  isAdmin: true,
+};
+const user = new User({ isAdmin: true });
+// console.log(user._doc);
+const token = user.generateAuthToken();
+console.log(token);
+
+const payload = jwt.verify(token, config.get("jwtPrivateKey"));
+console.log(payload);
 // const mongoose = require("mongoose");
 // const id = mongoose.Types.ObjectId();
 // console.log(id.getTimestamp());
